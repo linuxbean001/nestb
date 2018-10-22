79091ce3247bcc476236b1733e6193fdcebff3e7
linuxbean001 <linuxbean.hem@gmail.com>
2018-10-22 20:07:29 +0530

    update 22-10

---
+++ src/Header.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Navbar, NavItem, Nav, NavLink } from 'react-bootstrap';
+import Scrollchor from 'react-scrollchor';
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <header id="header" className="App-header">
+                    <Navbar inverse collapseOnSelect>
+                        <Navbar.Header>
+                            <Navbar.Brand>
+                                <a className="navbar-brand" href="#hero_banner"><img src="images/logo.png" /></a>
+                            </Navbar.Brand>
+                            <Navbar.Toggle />
+                        </Navbar.Header>
+                        <Navbar.Collapse>
+                            <Nav pullRight>
+                                <NavItem ><Scrollchor to="#about_us" className="nav-link">About Us</Scrollchor></NavItem>
+                                <NavItem><Scrollchor to="#platform" className="nav-link">Platform</Scrollchor></NavItem>
+                                <NavItem><Scrollchor to="#blogs" className="nav-link">Blogs</Scrollchor></NavItem>
+                                <NavItem ><Scrollchor to="#contact_us" className="nav-link">Contact Us</Scrollchor></NavItem>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+
+                </header>
+
+            </div>
+        );
+    }
+}
+
+export default Header;
